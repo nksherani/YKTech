dc185395c59a31c66b138f9b6b7c8e1020085c61
Shahzaibshzy <shhzbsaleem@gmail.com>
2026-01-08 23:36:53 +0500

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
-
 function App() {
   const [scrolled, setScrolled] = useState(false)
 
